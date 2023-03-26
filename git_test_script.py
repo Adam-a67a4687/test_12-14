5dcaaee616a7ced627f860223772c8fbe49d2e0b
Jan Kowalski <andszeszko@onet.eu>
2023-03-27 01:04:43 +0200

    removing "some_method" method

--- git_test_script.py
+++ git_test_script.py
@@ -7,9 +7,6 @@ class Transformator:
         y = 700
         return x + y
     
-    def some_method():
-        pass
-    
 if __name__ == '__main__':
     transformator = Transformator('WGS84')
     transformator.transform()
