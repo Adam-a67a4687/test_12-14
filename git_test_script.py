06bf6c448d7acdff038cf06c91153ea09ec2b7ea
Jan Kowalski <andszeszko@onet.eu>
2023-03-26 21:16:36 +0200

    adding __init__ with self.geoid and adding "if __name__ = __main__"

--- git_test_script.py
+++ git_test_script.py
@@ -1,4 +1,10 @@
 class Transformator:
+    def __init__(self, geoid: str) -> None:
+        self.geoid = geoid
 
     def transform():
         return 'dummy return'
+    
+if __name__ == '__main__':
+    transformator = Transformator('WGS84')
+    transformator.transform()
