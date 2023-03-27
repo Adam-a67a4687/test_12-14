282321ea5473ae9816b954d6f78c0e2b223345b5
Jan Kowalski <andszeszko@onet.eu>
2023-03-27 02:43:50 +0200

    refctoring "transform" and "display_geoid" methods

--- git_test_script.py
+++ git_test_script.py
@@ -2,14 +2,14 @@ class Transformator:
     def __init__(self, geoid: str) -> None:
         self.geoid = geoid
 
-    def transform():
+    def transform(self):
         x = 300
         y = 700
         return x + y
     
     def display_geoid(self):
         print(f'{self.geoid}')
-        return self.geoid
+        return f'This is our geoid: {self.geoid}'
     
 if __name__ == '__main__':
     transformator = Transformator('WGS84')
